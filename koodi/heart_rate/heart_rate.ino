070917dbcca64b260a781e73a1d26514bd722fdf
Svveri <severi.ronk@gmail.com>
2026-04-01 11:46:42 +0300

    Lisätty kooditiedostojen alut kansioon nimeltä koodi. Tiedostoissa ei vielä mitään koodia luotuna tässä vaiheessa

---
+++ koodi/heart_rate/heart_rate.ino
@@ -0,0 +1,12 @@
+// photoplethysmography heart rate monitor
+// using libraries available and own implementation if needed
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
