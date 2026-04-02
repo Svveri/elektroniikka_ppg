a7afe98a2f869db131e78ae1117086f72fb22f6f
Svveri <severi.ronk@gmail.com>
2026-04-02 10:45:46 +0300

    Validoitu signaalin mittaus DFROBOT_heartrate kirjastolla. Eli A0 pinniin kytkettynä, nappia paineltaessa kykenee mittaamaan signaalin ja printtaamaan sen serial monitorille.

--- koodi/heart_rate/heart_rate.ino
+++ koodi/heart_rate/heart_rate.ino
@@ -1,12 +1,17 @@
 // photoplethysmography heart rate monitor
 // using libraries available and own implementation if needed
+#include "DFRobot_Heartrate.h"
+#define heartPin A0
+
 
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(115200);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  int heartValue = analogRead(heartPin);
+  Serial.println(heartValue);
+  delay(20);
 }
